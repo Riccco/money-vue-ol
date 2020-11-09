b6669db688c8c80814cb3347cae074ccaa34892e
rico <5924435@qq.com>
2020-11-09 16:55:23 +0800

    deploy

---
+++ precache-manifest.df5c54b068553026b9bcadc51cbac772.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a60aa40437578df9bd2",
+    "url": "/money-vue-ol/css/app.7cd32d94.css"
+  },
+  {
+    "revision": "36599c2108687e9cae1f141d27d1ca8b",
+    "url": "/money-vue-ol/img/notFound.36599c21.png"
+  },
+  {
+    "revision": "7c9f98e62d57fcbea6bbd79cd14d0de7",
+    "url": "/money-vue-ol/index.html"
+  },
+  {
+    "revision": "6a60aa40437578df9bd2",
+    "url": "/money-vue-ol/js/app.17842330.js"
+  },
+  {
+    "revision": "6468843261614afcabf8",
+    "url": "/money-vue-ol/js/chunk-vendors.270c2393.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/money-vue-ol/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/money-vue-ol/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue-ol/robots.txt"
+  }
+]);
